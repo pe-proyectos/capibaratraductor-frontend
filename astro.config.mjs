521e1a55908a55f06ba4f89fb44fb2e5c67ce80d
Luis Choque <luis.choque.castro@outlook.com>
2024-07-15 23:17:32 -0500

    Updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    server: {
+      host: true
+    },
+    prefetch: false,
+    adapter: bun(),
     integrations: [
         react(),
         tailwind(),
